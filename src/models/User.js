71498096e00f5cefcf36d5661be07a5a296259b8
Secaly <kevin.ledieu.fr@gmail.com>
2017-11-25 05:11:00 +0100

    auth is working and adding token generation

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+  // TODO: add uniqueness and email validation
+const schema = new mongoose.Schema({
+  email: { type: String, required: true, lowercase: true, index: true },
+  passwordHash: { type: String, required: true }
+}, { timestamps: true });
+
+schema.methods.isValidPassword = function isValidPassword(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+}
+
+schema.methods.generateJMT = function generateJMT() {
+  return jwt.sign({
+    email: this.email
+  }, 'secretkey');
+}
+
+schema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    email: this.email,
+    token: this.generateJMT()
+  };
+}
+
+export default mongoose.model('User', schema);
